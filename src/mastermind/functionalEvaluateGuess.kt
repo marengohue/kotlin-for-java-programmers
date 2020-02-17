b7453f76ebd915b402c47f6ecabdca048c14a0d7
Eugene Rebedailo <mokonamodokidesu@gmail.com>
2020-02-17 14:54:21 +0300

    Added a tiny refactoring

--- src/mastermind/functionalEvaluateGuess.kt
+++ src/mastermind/functionalEvaluateGuess.kt
@@ -7,7 +7,7 @@ data class Evaluation(val rightPosition: Int, val wrongPosition: Int)
 fun evaluateGuess(secret: String, guess: String): Evaluation {
     val rightPositions = secret.zip(guess).count { (sl, gl) -> sl == gl }
     val commonLetters = ('A'..'F').sumBy { ch ->
-        min(secret.count { sl -> sl == ch }, guess.count { gl -> gl == ch })
+        min(secret.count { it == ch }, guess.count { it == ch })
     }
     return Evaluation(rightPositions, commonLetters - rightPositions)
 }
